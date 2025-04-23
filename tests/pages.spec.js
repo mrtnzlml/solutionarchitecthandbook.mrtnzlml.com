c03743de4b5951caf99dc6a0b203ef2fb8c82fa0
Martin Zlámal <mrtnzlml@gmail.com>
2025-04-23 20:59:27 +0200

    Simplify Playwright tests

--- tests/pages.spec.js
+++ tests/pages.spec.js
@@ -3,8 +3,8 @@ const { test, expect } = require('@playwright/test');
 test('homepage', async ({ page }) => {
   await page.goto('/');
 
-  await expect(page.getByRole('heading')).toContainText('Rossum.ai University');
-  await expect(page.getByRole('paragraph')).toContainText('Build something cool with Rossum.ai');
+  await expect(page.getByRole('heading')).toContainText('Unofficial Rossum.ai University');
+  await expect(page.getByRole('paragraph')).toContainText('Build something cool with Rossum.ai and @mrtnzlml');
 });
 
 test('main navigation (Learn button)', async ({ page }) => {
@@ -12,11 +12,4 @@ test('main navigation (Learn button)', async ({ page }) => {
 
   await page.getByLabel('Main', { exact: true }).getByRole('link', { name: 'Learn' }).click();
   await expect(page.locator('h1')).toContainText('AI training best practices');
-});
-
-test('main navigation (Watch button)', async ({ page }) => {
-  await page.goto('/');
-
-  await page.getByLabel('Main', { exact: true }).getByRole('link', { name: 'Watch' }).click();
-  await expect(page.locator('h2').first()).toContainText('Rossum e-invoicing for Germany');
 });
