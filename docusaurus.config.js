9b4234db7c1e0a86dfbf2f3b6fa984e5e7c45c40
Martin Zlámal <mrtnzlml@gmail.com>
2025-04-23 20:27:55 +0200

    Update docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -18,7 +18,7 @@ const config = {
   url: 'https://rossum.university',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: '/rossum.university/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
